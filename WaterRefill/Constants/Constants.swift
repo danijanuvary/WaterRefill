0c740c8770cce0399c4af9664aab148dda7df677
Jithin Syam <danijanuvary@gmail.com>
2022-05-19 14:03:40 +0530

    Changes testing

--- WaterRefill/Constants/Constants.swift
+++ WaterRefill/Constants/Constants.swift
@@ -21,4 +21,5 @@ class Constants: NSObject {
    static let KEY_LATITUDE = "currentlatitude"
    static let KEY_LONGITUDE = "currentlongitude"
    static let KEY_ORDER_BY = "order_by"
+   static let KEY_ORDER_BY_TEMP = "order_by" 
 }
